95bdca1085cd2e9843147ba393ece6f8115158e9
Christopher Mamian <christophermamian@gmail.com>
2015-02-10 20:59:45 -0800

    move snake function

--- scripts/application.js
+++ scripts/application.js
@@ -28,6 +28,95 @@ function draw_snake(){
 	SNAKE.push(580);
 }
 
+function move_snake(){
+	var currentMove = '';
+
+	setInterval(function() {
+	  if (currentMove !== '') {
+	    move(currentMove);
+	  }
+	}, 100);
+
+	$(document).keydown(function(event) {
+	  if (event.which === RIGHT) {
+	    currentMove = 'r';
+	  } else if (event.which === LEFT) {
+	    currentMove = 'l';
+	  } else if (event.which === UP) {
+	    currentMove = 'u';
+	  } else if (event.which === DOWN) {
+	    currentMove = 'd';
+	  }
+	});
+	
+	var move = function(dir){
+		var currentID = SNAKE[SNAKE.length-1];
+
+		switch(dir) {
+			case 'r':
+				var nextID = currentID+1;
+				break;
+			case 'l':
+				var nextID = currentID-1;
+				break;
+			case 'u':
+				var nextID = currentID-40;
+				break;
+			case 'd':
+				var nextID = currentID+40;
+				break;
+		}
+
+		var tail = 0;
+		//check whether the SNAKE has hit a 'fruit'
+		if ($("#"+nextID).hasClass("fruit")){
+			tail = SNAKE[0];
+			$("#"+nextID).removeClass('fruit');
+			genFruit();	 //generate a new fruit piece
+		}
+
+		//Check whether snake has hit itself
+		if ($("#"+nextID).hasClass("snake")){
+			alert('Game over!');
+			for (var i=0;i<=SNAKE.length;i++){
+				//remove snake from [i]
+				$("#"+SNAKE[i]).removeClass('snake');
+
+			}
+			SNAKE=[];
+			currentMove='';
+		}
+		//Check whether snake is out of bounds
+		if (!($("#"+nextID).hasClass("box"))){
+			alert('Game over!');
+			for (var i=0;i<=SNAKE.length;i++){
+				//remove snake from [i]
+				$("#"+SNAKE[i]).removeClass('snake');
+
+			}
+			SNAKE=[];
+			currentMove='';
+		}
+		//update SNAKE array
+		for (var i=0;i<SNAKE.length;i++){
+			//remove snake from [i]
+			$("#"+SNAKE[i]).removeClass('snake');
+			SNAKE[i]=SNAKE[i+1];
+		}
+		if (tail !== 0){
+			SNAKE.unshift(tail);
+		}
+
+		for (var i=0; i<SNAKE.length; i++){
+			$("#"+SNAKE[i]).addClass('snake');
+		}
+		SNAKE[SNAKE.length-1] = nextID;
+		$("#"+nextID).addClass('snakeHead');
+		$("#"+currentID).removeClass('snakeHead');	
+	};
+	
+}
+
 
 $(document).ready(function() {
 	draw_board(40,40);
